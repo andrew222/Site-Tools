819015e8e30e908854b37460763fcdef6906228e
Andrew Yang <zzcv20051122@gmail.com>
2014-07-13 16:42:32 +0800

    MISC - Remove useless actions in broken_links_controller.rb

--- app/controllers/broken_links_controller.rb
+++ app/controllers/broken_links_controller.rb
@@ -1,20 +1,5 @@
 class BrokenLinksController < ApplicationController
-  # GET /broken_links
-  # GET /broken_links.json
   before_filter :check_site
-
-  def index
-    @broken_links = current_user
-                      .sites
-                        .find(params[:site_id])
-                          .broken_links
-                            .where(:link_type => params[:link_type])
-                              .paginate(:page => params[:page], per_page: 15)
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render :partial => "broken_links" }
-    end
-  end
 
   # GET /broken_links/1
   # GET /broken_links/1.json
@@ -27,21 +12,9 @@ class BrokenLinksController < ApplicationController
     end
   end
 
-  # GET /broken_links/new
-  # GET /broken_links/new.json
-  def new
-    
-  end
-
   # GET /broken_links/1/edit
   def edit
     @broken_link = @site.broken_links.find(params[:id])
-  end
-
-  # POST /broken_links
-  # POST /broken_links.json
-  def create
-    
   end
 
   # PUT /broken_links/1
